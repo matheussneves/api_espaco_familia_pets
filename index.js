11b005a5196ea36cacb5842c67782a34f7e0eaae
matheussolanoneves <matheus.neves@webmotors.com.br>
2024-05-17 12:15:58 -0300

    j

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./src/routes/routes');
 const app = express();
+const porta = 80
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +12,8 @@ app.get('/', (request, response) => {
     response.send("autenticação feita com sucesso");
 });
 
-app.listen(4000, () => {
-    console.log("Aplicação rodando na porta 4000");
+app.listen(porta, () => {
+    console.log("Aplicação rodando na porta " + porta);
 });
 
 
